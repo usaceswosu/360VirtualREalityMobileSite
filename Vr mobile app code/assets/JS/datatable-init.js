e1b1f745344a4b84a775bd62d12dac44467f52df
Dillon Dell Laptop <martinezdd@student.swosu.edu>
2025-09-25 12:47:21 -0500

    Commented off unnecessary functions in data table. Added USACE logo in footer-right w/ cropping edits

--- Vr mobile app code/assets/JS/datatable-init.js
+++ Vr mobile app code/assets/JS/datatable-init.js
@@ -5,16 +5,16 @@ $(document).ready(function() {
         pageLength: 10,
         lengthMenu: [5, 10, 25, 50],
         order: [[1]],
-        language: {
-            search: "Search Projects:",
-            lengthMenu: "Show _MENU_ projects per page",
-            info: "Showing _START_ to _END_ of _TOTAL_ projects",
-            paginate: {
-                first: "First",
-                last: "Last",
-                next: "Next",
-                previous: "Previous"
-            }
+        //language: {
+            //search: "Search Projects:",
+            //lengthMenu: "Show _MENU_ projects per page",
+            //info: "Showing _START_ to _END_ of _TOTAL_ projects",
+            //paginate: {
+                //first: "First",
+                //last: "Last",
+                //next: "Next",
+                //previous: "Previous"
+            //}
         }
     });
 });
