98acab491b99547a814aa4079adda74c85a9d4d9
Ann797 <groichera@student.swosu.edu>
2025-10-07 13:14:14 -0500

    Each Lake a modal, all are currently empty

--- Vr mobile app code/assets/JS/datatable-init.js
+++ Vr mobile app code/assets/JS/datatable-init.js
@@ -18,3 +18,21 @@ $(document).ready(function() {
         }
     });
 });
+
+
+//Modal Content Change -- in progress
+const myModal = document.querySelector('#universalModal');
+    myModal.addEventListener('show.bs.modal', function(event) {
+        // Get the reference of the triggering button
+        const button = event.relatedTarget;
+
+        // Get the data for inserting into modal
+        const heading = button.getAttribute('data-bs-heading');
+        const modalcontent = button.getAttribute('data-bs-modalcontent');
+        const footercontent = button.getAttribute('data-bs-footercontent');
+
+        // Set the value in the modal
+        myModal.querySelector('.modal-title').textContent = heading;
+        myModal.querySelector('.modal-body').textContent = modalcontent;
+        myModal.querySelector('.modal-footer').textContent = footercontent;
+    });
