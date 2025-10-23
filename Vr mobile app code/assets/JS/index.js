a22cc70c9639e6be36547dee36fb8805a0bc15a9
Ann797 <groichera@student.swosu.edu>
2025-10-23 17:56:57 -0500

    included Sky's thumbnails and javascript/button functions

--- Vr mobile app code/assets/JS/index.js
+++ Vr mobile app code/assets/JS/index.js
@@ -1,20 +1,16 @@
-const myModal = document.querySelector('#universalModal');
-    myModal.addEventListener('show.bs.modal', function(event) {
-        // Get the reference of the triggering button
-        const button = event.relatedTarget;
+//PROBABLY DONT NEED ENTIRELY
+const container = document.querySelector('.thumb-grid');
 
-        // Get the data for inserting into modal
-        const heading = button.getAttribute('data-bs-heading');
-        const modalcontent = button.getAttribute('data-bs-modalcontent');
-        //const modalcontent = button.getAttribute(if('data-bs-modalcontent-beaverlake'){
-        //    'data-bs-modalcontent-beaverlake'
-        //} else{
-        //    'data-bs-modalcontent'
-        //});
+thumbnails.forEach(item => {
+  const btn = document.createElement('button');
+  btn.className = 'thumb-btn';
+  btn.innerHTML = `<img src="${item.src}" alt="${item.name}">`;
 
-        // Set the value in the modal
-        myModal.querySelector('.modal-title').textContent = heading;
-        myModal.querySelector('.modal-body').textContent = modalcontent;
-        
-    });
+  btn.addEventListener('click', () => {
+    document.querySelectorAll('.thumb-btn').forEach(b => b.classList.remove('selected'));
+    btn.classList.add('selected');
+    item.action();
+  });
 
+  container.appendChild(btn);
+});
---
+++ Vr mobile app code/assets/JS/thumbnail_list.js
@@ -0,0 +1,19 @@
+const thumbnails = imageList.map(name => {
+  //change images/ file path to function
+  const safeSrc = "images/" + encodeURIComponent(name);
+  return {
+    name: name.replace(".png", ""),
+    src: safeSrc,
+    action: () => alert(`${name} clicked!`)
+  };
+});
+
+const container = document.querySelector('.thumb-grid');
+
+thumbnails.forEach(item => {
+  const btn = document.createElement('button');
+  btn.className = 'thumb-btn';
+  btn.innerHTML = `<img src="${item.src}" alt="${item.name}">`;
+  btn.onclick = item.action;
+  container.appendChild(btn);
+});
